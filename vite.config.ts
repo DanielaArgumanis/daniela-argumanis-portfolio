95599d81292c5a7abce97b4314375a6ea21ebe3b
DanielaArgumanis <daniela.argumanis@gmail.com>
2023-12-04 15:50:30 -0500

    💩 Needs Improvement:  Try to fix deploy

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import svgr from 'vite-plugin-svgr';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), svgr()],
+  publicDir: 'public',
   resolve: {
     alias: {
       '@commonComponents': '/src/common/components',
